729ddcb4db09e47bacafc6fc36909050a4f7a1c8
SanjayVarun <sanjay.varun95@gmail.com>
2017-03-23 22:23:26 +0530

    Remove files irrelevant to project

--- AngularJS/javascript/sample.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("myApp" , []);
-app.controller("myCtrl" , function($scope) {
-	$scope.test = "lightgreen";
-	$scope.myStyle = {
-		"color" :	"red",
-		"font-family" :	"verdana",
-		"font-size" :	"34px"
-	}
-});
